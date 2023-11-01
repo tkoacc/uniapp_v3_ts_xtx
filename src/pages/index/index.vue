93eb67b3aa498691abf1357fe16b852c9b41d704
TOP-Z\riley <riley211375@gmail.com>
2023-11-01 14:58:01 +0800

    feat: 轮播图指示点

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,8 +3,10 @@ import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <custom-navbar />
-
+  <!-- 自定义轮播图 -->
+  <XtxSwiper />
   <view class="index">index</view>
 </template>
 
