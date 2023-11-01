7695f368ce3e26d3037ea58ca8c8e39399b01453
TOP-Z\riley <riley211375@gmail.com>
2023-11-02 00:22:03 +0800

    feat: 猜你喜欢类型定义和列表渲染

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,4 +1,6 @@
-import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem, GuessItem } from '@/types/home'
+import type { PageResult } from '@/types/global'
+
 import { http } from '@/utils/http'
 /**
  * 首页-广告区域-小程序
@@ -31,5 +33,14 @@ export const getHomeHotAPI = () => {
   return http<HotItem[]>({
     method: 'GET',
     url: '/home/hot/mutli',
+  })
+}
+/**
+ * 猜你喜欢-小程序
+ */
+export const getHomeGoodsGuessLikeAPI = () => {
+  return http<PageResult<GuessItem>>({
+    method: 'GET',
+    url: '/home/goods/guessLike',
   })
 }
