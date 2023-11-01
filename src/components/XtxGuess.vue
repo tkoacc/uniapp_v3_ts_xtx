72a76e6fb2fb111f412df37c9797122ee4d88f67
TOP-Z\riley <riley211375@gmail.com>
2023-11-02 02:37:52 +0800

    feat: 下拉刷新 猜你喜欢数据重置

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -28,7 +28,12 @@ const getHomeGoodsGuessLikeData = async () => {
     finish.value = true
   }
 }
-
+// 重置数据
+const resetData = () => {
+  pageParams.page = 1
+  guessList.value = []
+  finish.value = false
+}
 // 组件挂载完毕
 onMounted(() => {
   getHomeGoodsGuessLikeData()
@@ -37,6 +42,7 @@ onMounted(() => {
 // 暴露方法
 defineExpose({
   getMore: getHomeGoodsGuessLikeData,
+  resetData,
 })
 </script>
 
