936ff402bb28f311bd152f0d622ea3b0c2c4990c
TOP-Z\riley <riley211375@gmail.com>
2023-11-01 21:07:29 +0800

    feat: 猜你喜欢组件封装

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -1,11 +1,10 @@
-/**
- * declare module '@vue/runtime-core'
- *   现调整为
- * declare module 'vue'
- */
-import 'vue'
+// src/types/components.d.ts
+import XtxSwiper from '@/components/XtxSwiper.vue'
+import XtxGuess from '@/components/XtxGuess.vue'
+
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
   }
 }
