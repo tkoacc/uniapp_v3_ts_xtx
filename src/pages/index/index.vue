2a9824c44c88f44facaf23ce2b4f6e70ab705eb5
TOP-Z\riley <riley211375@gmail.com>
2023-11-01 17:40:39 +0800

    feat: 前台分类组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import CategoryPanel from '@/pages/index/components/CategoryPanel.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import { getHomeBannerAPI } from '@/services/home'
@@ -20,9 +21,13 @@ onLoad(() => {
   <custom-navbar />
   <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
