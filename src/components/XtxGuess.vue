d72e7fe2b64ce49d1fea86dd35e9929249b18114
TOP-Z\riley <riley211375@gmail.com>
2023-11-02 02:10:32 +0800

    feat: 猜你喜欢 分页条件

--- src/components/XtxGuess.vue
+++ src/components/XtxGuess.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 import { getHomeGoodsGuessLikeAPI } from '@/services/home'
+import type { PageParams } from '@/types/global'
 import type { GuessItem } from '@/types/home'
 import { onMounted, ref } from 'vue'
 
+// 分页参数
+const pageParams: Required<PageParams> = {
+  page: 1,
+  pageSize: 10,
+}
 // 获取猜你喜欢数据
 const guessList = ref<GuessItem[]>([])
+// 已结束标记
+const finish = ref(false)
 const getHomeGoodsGuessLikeData = async () => {
-  const res = await getHomeGoodsGuessLikeAPI()
-  guessList.value = res.result.items
+  // 退出判断
+  if (finish.value) return uni.showToast({ title: '没有更多了', icon: 'none' })
+  const res = await getHomeGoodsGuessLikeAPI(pageParams)
+  // guessList.value = res.result.items
+  // 数组的追加
+  guessList.value.push(...res.result.items)
+  // 分页条件
+  if (pageParams.page < res.result.pages) {
+    // 页码累加
+    pageParams.page++
+  } else {
+    finish.value = true
+  }
 }
 
 // 组件挂载完毕
@@ -41,7 +60,7 @@ defineExpose({
       </view>
     </navigator>
   </view>
-  <view class="loading-text"> 正在加载... </view>
+  <view class="loading-text"> {{ finish ? '没有更多了' : '正在加载...' }} </view>
 </template>
 
 <style lang="scss">
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,5 +1,5 @@
 import type { BannerItem, CategoryItem, HotItem, GuessItem } from '@/types/home'
-import type { PageResult } from '@/types/global'
+import type { PageParams, PageResult } from '@/types/global'
 
 import { http } from '@/utils/http'
 /**
@@ -38,9 +38,10 @@ export const getHomeHotAPI = () => {
 /**
  * 猜你喜欢-小程序
  */
-export const getHomeGoodsGuessLikeAPI = () => {
+export const getHomeGoodsGuessLikeAPI = (data?: PageParams) => {
   return http<PageResult<GuessItem>>({
     method: 'GET',
     url: '/home/goods/guessLike',
+    data,
   })
 }
