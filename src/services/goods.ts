72fa3613770f61192873d0cd25873506aa444fed
TOP-Z\riley <riley211375@gmail.com>
2023-11-03 01:00:59 +0800

    feat: 商品详情 页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,10 +1,11 @@
+import { GoodsResult } from './../types/goods.d'
 import { http } from '@/utils/http'
 /**
  * 获取商品详情
  * @param id
  */
 export const getGoodsByIdAPI = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: `/goods`,
     data: {
