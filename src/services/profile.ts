a9bc214d10d60d7770f60b83346b798d808c9338
TOP-Z\riley <riley211375@gmail.com>
2023-11-17 00:30:40 +0800

    feat: 个人信息 修改用户昵称

--- src/services/profile.ts
+++ src/services/profile.ts
@@ -1,9 +1,24 @@
+import { ProfileParams } from './../types/member.d'
 import type { ProfileDetail } from '@/types/member'
 import { http } from '@/utils/http'
 
+/**
+ * 获取个人信息
+ */
 export const getMemberProfileAPI = () => {
   return http<ProfileDetail>({
     method: 'GET',
     url: '/member/profile',
+  })
+}
+
+/**
+ * 修改个人信息
+ */
+export const putMemberProfileAPI = (data: ProfileParams) => {
+  return http({
+    method: 'PUT',
+    url: '/member/profile',
+    data,
   })
 }
